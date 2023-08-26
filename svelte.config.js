72b755851b03e9f5c890b01d024a95823f38d2a1
Tomas Galle <tomas.galle@ugent.be>
2023-08-26 11:44:59 +0200

    :recycle: Change config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -14,7 +16,7 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			// change below to your repo name
-			base: process.env.NODE_ENV === 'production' ? '/togalle.github.io' : ''
+			base: dev ? '' : process.env.BASE_PATH,
 		},
 	}
 };
